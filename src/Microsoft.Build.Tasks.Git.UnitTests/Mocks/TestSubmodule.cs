d8c5bdd894c8ebf1df69d1cd510450a5e9fd9a62
Tomas Matousek <tomat@microsoft.com>
2018-04-18 09:26:00 -0700

    Move mocks to separate files

---
+++ src/Microsoft.Build.Tasks.Git.UnitTests/Mocks/TestSubmodule.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using LibGit2Sharp;
+
+namespace Microsoft.Build.Tasks.Git.UnitTests
+{
+    internal class TestSubmodule : Submodule
+    {
+        private readonly string _url;
+        private readonly string _path;
+        private readonly ObjectId _workDirCommitSha;
+
+        public TestSubmodule(string url, string path, string workDirCommitSha)
+        {
+            _url = url;
+            _path = path;
+            _workDirCommitSha = new ObjectId(workDirCommitSha);
+        }
+
+        public override string Url => _url;
+        public override string Path => _path;
+        public override ObjectId WorkDirCommitId => _workDirCommitSha;
+    }
+}
